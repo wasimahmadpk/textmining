4f35507c681eec4c6cd9370239dd6b8455e82cb0
Wasim <wasimahmad.qc@gmail.com>
2019-10-02 23:53:35 +0200

    Normalization file added

---
+++ Normalization.py
@@ -0,0 +1,17 @@
+# Stemming 
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+
+ps = PorterStemmer
+stemmed_words = []
+text = "I am flying better than birds ;)"
+tokenized_words = word_tokenize(text)
+
+for w in tokenized_words:
+
+    stemmed_words.append(ps.stem("flying"))
+
+print("Tokenize words",  tokenized_words)
+print("Stemmed words", stemmed_words)
