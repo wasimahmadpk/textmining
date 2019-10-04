3cd9e5ae6427bd4c42f5a54c8c98367646970283
Wasim <wasimahmad.qc@gmail.com>
2019-10-05 01:19:55 +0200

    changes in sentimentanalysis

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,17 +1,37 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 
 # read train data
+data = pd.read_csv(r'F:\AppliedAI\NLPpro\textmining\datasets\train.tsv', sep='\t')
 
-train_data = pd.read_csv(r'F:\AppliedAI\NLPpro\textmining\datasets\train.tsv', sep='\t')
+print(data.head(5))
+print(data.info())
 
-print(train_data.head(5))
-print(train_data.info())
+print(data.Sentiment.value_counts())
 
-print(train_data.Sentiment.value_counts())
+# plot user responses
+Sentiment_count = data.groupby('Sentiment').count()
+# plt.bar(Sentiment_count.index.values, Sentiment_count['Phrase'])
+# plt.xlabel('Review Sentiments')
+# plt.ylabel('Number of Review')
+# plt.show()
 
-Sentiment_count = train_data.groupby('Sentiment').count()
-plt.bar(Sentiment_count.index.values, Sentiment_count['Phrase'])
-plt.xlabel('Review Sentiments')
-plt.ylabel('Number of Review')
-plt.show()
+
+# tokenizer to remove unwanted elements from out data like symbols and numbers
+token = RegexpTokenizer(r'[a-zA-Z0-9]+')
+cv = CountVectorizer(lowercase=True,stop_words='english', ngram_range = (1, 1), tokenizer = token.tokenize)
+text_counts= cv.fit_transform(data['Phrase'])
+
+# split train and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    text_counts, data['Sentiment'], test_size=0.3, random_state=1)
+
+# Model Generation Using Multinomial Naive Bayes
+clf = MultinomialNB().fit(X_train, y_train)
+predicted= clf.predict(X_test)
+print("MultinomialNB Accuracy:",metrics.accuracy_score(y_test, predicted))
