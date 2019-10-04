fc25321199bf2e7b257672572ec6e0f5f0b1c7ea
Wasim <wasimahmad.qc@gmail.com>
2019-10-05 00:48:09 +0200

    Sentiment Analysis updated

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 # read train data
 
@@ -6,3 +7,11 @@ train_data = pd.read_csv(r'F:\AppliedAI\NLPpro\textmining\datasets\train.tsv', s
 
 print(train_data.head(5))
 print(train_data.info())
+
+print(train_data.Sentiment.value_counts())
+
+Sentiment_count = train_data.groupby('Sentiment').count()
+plt.bar(Sentiment_count.index.values, Sentiment_count['Phrase'])
+plt.xlabel('Review Sentiments')
+plt.ylabel('Number of Review')
+plt.show()
